0557463f2ac13f162b129a358e7d1d70ca609992
Liza <skomorohova-elizaveta@rambler.ru>
2023-08-09 18:59:50 +0300

    test successful

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -3,9 +3,10 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoTest {
     @Test
-    void test(){
+    void test() {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
@@ -15,8 +16,7 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equals("some data"))
+                .body("data", equalTo("some data"))
         ;
     }
-
 }
